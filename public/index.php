e2a88a00706d8ce85db4217fbf41038dc18ef37e
Tran Trung Nhan <bonhan1802@gmail.com>
2026-03-04 11:20:58 +0700

    add route handlings

--- public/index.php
+++ public/index.php
@@ -2,39 +2,43 @@
 require_once __DIR__ . '/../autoload.php';
 
 use App\Core\Session;
-use App\Controllers\AuthController;
+use App\Controllers\ErrorController;
+use App\Controllers\FileController;
 
 Session::start();
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-switch ($requestUri) {
-    case '/':
-        if (!Session::get('user_id')) {
-            header('Location: /login');
-            exit;
-        }
-        $fullName = Session::get('full_name', 'User');
-        $role = Session::get('role', 'student');
-        require_once __DIR__ . '/../src/Views/home.php';
-        break;
-    case '/login':
-        $authController = new AuthController();
-        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            $authController->showLogin();
-        } else {
-            $authController->login();
-        }
-        break;
-    case '/logout':
-        $authController = new AuthController();
-        $authController->logout();
-        break;
-    default:
-        http_response_code(404);
-        echo "Page not found";
-        echo "Route: " . htmlspecialchars($requestUri);
-        break;
+if(preg_match('#^/api/files/(.+)$#', $requestUri, $matches)) {
+    $filename = $matches[1];
+    $fileController = new FileController();
+    $fileController->serveAvatar($filename);
+    exit;
 }
 
-?>
+// Mảng 2 chiều map các route và controller 
+$routes = [
+    'GET' => [
+        '/' => ['controller' => 'App\Controllers\HomeController', 'method' => 'index'],
+        '/profile' => ['controller' => 'App\Controllers\ProfileController', 'method' => 'showProfile'],
+        '/login' => ['controller' => 'App\Controllers\AuthController', 'method' => 'showLogin'],
+        '/logout' => ['controller' => 'App\Controllers\AuthController', 'method' => 'logout'],
+    ],
+    'POST' => [
+        '/login' => ['controller' => 'App\Controllers\AuthController', 'method' => 'login'],
+        '/profile' => ['controller' => 'App\Controllers\ProfileController', 'method' => 'updateProfile']
+    ]
+];
+
+if (isset($routes[$requestMethod][$requestUri])) {
+    $target = $routes[$requestMethod][$requestUri];
+    $className = $target['controller'];
+    $methodName = $target['method'];
+    
+    $controller = new $className();
+    $controller->$methodName();
+} else {
+    $errorController = new ErrorController();
+    $errorController->notFound();
+}
